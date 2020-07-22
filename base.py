8194211adb5d152f9e3da6a74acef78662fd775b
Gaurav Singh <gaurav.singh@go-jek.com>
2020-07-22 22:15:20 +0530

    Initial commit with a basic flask app to serve hello world page

---
+++ base.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
