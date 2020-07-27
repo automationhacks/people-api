d8f1f18944a3c7adb8489f976dd72200fccaf81b
Gaurav Singh <gaurav.singh@go-jek.com>
2020-07-27 07:00:25 +0530

    Added impl for create, read_one and update operations for people API

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 Dummy REST API created using Flask, Connexion, Swagger and SQL Alchemy
 
+This follows the steps in [Read python tut on Flask connexion Rest API](https://realpython.com/flask-connexion-rest-api/#what-rest-is)
 
-## Gotchas
+## Setup
+
+- Ensure you have pipenv available. Read this [blog](https://automationhacks.blog/2020/07/12/how-to-manage-your-python-virtualenvs-with-pipenv/) to understand all about pipenv
+- Execute `pipenv shell`
+- cd to `people-api`
+- Execute `python server.py`
+- To open swagger navigate to `http://0.0.0.0:5000/api/ui/`
+
+## Project structure
+
+- `server.py` has the code to start the Flask app with connexion
+- `people.py` has the implementation for CRUD operations of the people API
+- `swagger.yml` has the swagger spec to define the route for the API while also allowing to build out a nice swagger documentation
+
+## Common Gotchas
 
 - To install swagger-ui, please run `pipenv install "connexion[swagger-ui]"`. Read this [bug](https://github.com/zalando/connexion/issues/779) to understand about why zsh needs this to be quoted.
-
-## References
-
-This follows the steps in [Read python tut on Flask connexion Rest API](https://realpython.com/flask-connexion-rest-api/#what-rest-is)
--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import make_response, abort
 
 
 def get_timestamp():
@@ -6,27 +7,58 @@ def get_timestamp():
 
 
 PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp()
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp()
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp()
-    }
+    "Farrell": {"fname": "Doug", "lname": "Farrell", "timestamp": get_timestamp()},
+    "Brockman": {"fname": "Kent", "lname": "Brockman", "timestamp": get_timestamp()},
+    "Easter": {"fname": "Bunny", "lname": "Easter", "timestamp": get_timestamp()},
 }
 
 
 def read():
     """
-    responds to /api/people call
+    GET /api/people 
     :return:
     """
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+
+
+def create(person):
+    """
+    creates a person in people dict based on passed in person data
+    PUT /api/people
+    """
+    lname = person.get("lname", None)
+    fname = person.get("fname", None)
+
+    if lname not in PEOPLE and lname is not None:
+        PEOPLE[lname] = {"lname": lname, "fname": fname, "timestamp": get_timestamp()}
+
+        return make_response(f"{lname} successfully created", 201)
+    else:
+        abort(f"Person with {lname} already exists")
+
+
+def read_one(lname):
+    """
+    Read a person by lname
+    GET /api/people/{lname}
+    """
+    if lname in PEOPLE:
+        return PEOPLE.get(lname)
+    else:
+        abort(404, f"Person with last name {lname} not found")
+    return {}
+
+
+def update(lname, person):
+    """
+    Update person with lname with new first/last name
+    PUT /api/people/{lname}
+    """
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname")
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
